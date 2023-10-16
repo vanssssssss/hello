10f213f7fa6a7a3a621d663d59de6f7bdeaa5943
vanssssssss <148024757+vanssssssss@users.noreply.github.com>
2023-10-16 08:09:10 +0530

    Add files via upload

---
+++ ascii.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    char a;
+    printf("enter a character\n");
+    scanf("%c", &a);
+    printf("the ascii value of %c is %d",a,a);
+    getch();
+}
